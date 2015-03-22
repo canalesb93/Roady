a245d0144d1c0c17e27aeced0a187ad9d912b6c9
Ricardo Canales <canalesb93@gmail.com>
2015-03-22 11:16:45 -0600

    Changes friends to all users on the page

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,16 @@
 class API::UsersController < API::APIController
 
   def friends
-    graph = Koala::Facebook::API.new(current_user.access_token)
-    friends = []
-    graph.get_connections("me", "friends").each do |friend|
-      byebug
-      user = User.find_by(uid: friend["id"])
-      if user
-        friends << user 
-      end
-    end
-    render json: friends, status: :ok
+    # graph = Koala::Facebook::API.new(current_user.access_token)
+    # friends = []
+    # graph.get_connections("me", "friends").each do |friend|
+    #   byebug
+    #   user = User.find_by(uid: friend["id"])
+    #   if user
+    #     friends << user 
+    #   end
+    # end
+    render json: User.all, status: :ok
   end
 
   def current_race
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,7 @@ Rails.application.routes.draw do
     get 'users/current_race', to: 'users#current_race'
     post 'users/accept_race', to: 'users#accept_race'
     post 'races/exit_race', to: 'races#exit_race'
-    
   end
-
   resources :users
+
 end
